cfc7237557c99092b1f00d37888d260cfde27382
Denisse Chagoya <denissechagoyaveloz@gmail.com>
2026-06-11 10:00:14 -0600

    Simplify Vercel deployment

--- backend/src/swagger.js
+++ backend/src/swagger.js
@@ -1,9 +1,10 @@
 const swaggerJsdoc = require("swagger-jsdoc");
+const path = require("path");
 const openApiDocument = require("./docs/openapi");
 
 const options = {
   definition: openApiDocument,
-  apis: ["./src/routes/*.js"]
+  apis: [path.join(__dirname, "routes/*.js")]
 };
 
 module.exports = swaggerJsdoc(options);
